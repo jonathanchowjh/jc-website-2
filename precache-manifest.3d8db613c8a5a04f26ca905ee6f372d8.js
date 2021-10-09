b42aee9ba09764bf64d634795d81e6ff50853c8e
Jonathan Chow <jonathanchowjh@gmail.com>
2021-10-09 17:21:53 +0800

    Updates

---
+++ precache-manifest.3d8db613c8a5a04f26ca905ee6f372d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba3b14bd005503fd66a35437a86a546",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a48d908caf746851d2d",
+    "url": "/static/css/2.77b5a5a5.chunk.css"
+  },
+  {
+    "revision": "1b34c808cf3c07d87ae4",
+    "url": "/static/css/main.da3e029d.chunk.css"
+  },
+  {
+    "revision": "5a48d908caf746851d2d",
+    "url": "/static/js/2.607114b8.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.607114b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e498133da6880a462e",
+    "url": "/static/js/3.444faeeb.chunk.js"
+  },
+  {
+    "revision": "1b34c808cf3c07d87ae4",
+    "url": "/static/js/main.e8d9be76.chunk.js"
+  },
+  {
+    "revision": "031896a1e4a62a5e9c4c",
+    "url": "/static/js/runtime-main.2a48755f.js"
+  }
+]);
